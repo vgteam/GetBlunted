df190fb6b402f7a67d023be8ed69eddd4cf697fc
ryan <rlorigro@ucsc.edu>
2020-07-14 12:17:57 -0700

    Added some of vgteam/vg/algorithms

--- src/test/test_bdsg.cpp
+++ src/test/test_bdsg.cpp
@@ -1,6 +1,8 @@
 #include "bdsg/packed_graph.hpp"
 #include "gfa_to_handle.hpp"
 #include "utility.hpp"
+#include "topological_sort.hpp"
+#include "IncrementalIdMap.hpp"
 
 
 using std::ifstream;
@@ -10,6 +12,8 @@ using bluntifier::gfa_to_path_handle_graph;
 using bluntifier::gfa_to_handle_graph;
 using bluntifier::parent_path;
 using bluntifier::join_paths;
+using bluntifier::orient_nodes_forward;
+using bluntifier::IncrementalIdMap;
 using handlegraph::handle_t;
 using bdsg::PackedGraph;
 using bdsg::MutablePathMutableHandleGraph;
@@ -20,19 +24,19 @@ int main(){
     string project_directory = parent_path(script_path, 3);
 
     // Get test GFA path
-    string relative_gfa_path = "/data/test_gfa1.gfa";
+    string relative_gfa_path = "/data/diploid_case_d.gfa";
     const string absolute_gfa_path = join_paths(project_directory, relative_gfa_path);
     ifstream file(absolute_gfa_path);
 
     PackedGraph g;
+    IncrementalIdMap id_map;
 
-    gfa_to_handle_graph(absolute_gfa_path, g);
-
+    gfa_to_handle_graph(absolute_gfa_path, g, id_map);
     g.clear();
 
-    gfa_to_path_handle_graph_in_memory(file, g);
+    gfa_to_path_handle_graph_in_memory(file, g, id_map);
 
-
+    orient_nodes_forward(g);
 
 
 
