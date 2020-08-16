4a3e3e03a81fa690da752dfbbecdedb0a2ce777a
Jordan Eizenga <joeizeng@gmail.com>
2020-08-15 19:30:57 -0700

    implement domino free checking algorithm

--- inc/biclique_cover.hpp
+++ inc/biclique_cover.hpp
@@ -11,6 +11,7 @@
 #include <functional>
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>
 
 #include "handlegraph/handle_graph.hpp"
 #include "handlegraph/types.hpp"
@@ -25,12 +26,16 @@ using std::unordered_map;
 using std::function;
 using handlegraph::HandleGraph;
 
+/*
+ * Represents an instance of the minimum biclique cover problem for a
+ * bipartite subgraph of a larger graph
+ */
 class BicliqueCover {
 public:
     
     // initialize with a graph and partition of node sides. the
     // subgraph induced by the partition must be bipartite to be
-    // valie (this is not checked)
+    // valid (this is not checked)
     BicliqueCover(const HandleGraph& graph,
                   const bipartition& partition);
     ~BicliqueCover();
@@ -40,33 +45,63 @@ public:
     // subset of the nodes
     vector<bipartition> get() const;
     
-    
-    bool is_domino_free() const;
-    
 private:
     
-    // TODO: redundant with adjacency component
+    struct GaloisLattice {
+        GaloisLattice();
+        ~GaloisLattice();
+        
+        void separator();
+    }
     
     // lambda returns true if iteration should continue. function returns
     // true if iteration was not stopped early by lambda.
+    // TODO: redundant with adjacency component
     bool for_each_adjacent_side(const handle_t& side,
                                 const function<bool(handle_t)>& lambda) const;
     
     void simplify_side(const vector<handle_t>& simplifying_side,
                        SubtractiveHandleGraph& simplifying) const;
     
-    
+    // Amilhastre, et al. 1998, algorithm 2. remove edges without affecting
+    // the biclique cover
     SubtractiveHandleGraph simplify() const;
     
-    vector<bipartition> domino_free_cover(const HandleGraph& simple_graph) const;
+    // apply local checking (Amilhastre, et al. 1998, algorithm 3) to all
+    // nodes in order to check if the bipartite graph is domino-free
+    bool is_domino_free() const;
+    
+    vector<bipartition> domino_free_cover() const;
     
     vector<bipartition> heuristic_cover() const;
+    
+    GaloisLattice get_galois_lattice(const HandleGraph& simple_graph) const;
     
     const HandleGraph& graph;
     vector<handle_t> left_partition;
     vector<handle_t> right_partition;
     unordered_map<handle_t, size_t> left_partition_index;
     unordered_map<handle_t, size_t> right_partition_index;
+    
+    friend class QuotientBall;
+};
+
+/*
+ * Represents the quotient graph of two-hop subgraph starting at a center
+ * node over the equivalence relationship of having the same neighborhood
+ * (applied to nodes on the same side as the center)
+ */
+class QuotientBall {
+public:
+    QuotientBall(const BicliqueCover& parent, handle_t center);
+    QuotientBall() = delete;
+    ~QuotientBall() = default;
+    
+    // Amilhastre, et al (1998) algorithm 3
+    bool check_neighbor_ordering_property() const;
+    
+private:
+    vector<vector<size_t>> left_edges, right_edges;
 };
 
 
--- src/biclique_cover.cpp
+++ src/biclique_cover.cpp
@@ -8,6 +8,7 @@
 namespace bluntifier {
 
 using std::unordered_set;
+using std::sort;
 
 BicliqueCover::BicliqueCover(const HandleGraph& graph,
                              const bipartition& partition)
@@ -42,23 +43,166 @@ vector<bipartition> BicliqueCover::get() const {
     // TODO: magic number
     if (edge_count * (left_partition.size() + right_partition.size()) <= 65536
         && is_domino_free()) {
-        
-        // simplify the graph without affecting the biclique cover (Amilhastre, et al.
-        // 1998 algorithm 2).
-        SubtractiveHandleGraph simplified = simplify();
-        
-        //
-        return_val = domino_free_cover(simplified);
+        // compute the biclique cover on the simplified graph using the algorithm
+        // of Amilhastre, et al. 1998
+        return_val = domino_free_cover();
     }
     else {
+        // use the heuristic algorithm of Ene, et al. 2008
         return_val = heuristic_cover();
     }
     return return_val;
 }
 
+bool QuotientBall::QuotientBall(const BicliqueCover& super, handle_t center) const {
+    
+    // get the two-hop subgraph starting at the center
+    unordered_map<handle_t, size_t> left_idx;
+    // we have to restrict rightward edges since some of them could
+    // point outside the subgraph
+    vector<vector<size_t>> unrefined_left_edges;
+    
+    super.for_each_adjacent_side(center, [&](handle_t right) {
+        super.for_each_adjacent_side(right, [&](handle_t left) {
+            auto f = left_idx.find(left);
+            if (f == left_idx.end()) {
+                left_idx[left] = unrefined_left_edges.size();
+                unrefined_left_edges.emplace_back(1, right_edges.size());
+            }
+            else {
+                unrefined_left_edges[f->second].push_back(right_edges.size());
+            }
+            return true;
+        });
+        right_edges.emplace_back();
+        right_nodes.emplace_back(right);
+        return true;
+    });
+    
+    // initialize every node on the left in the same equivalence class
+    vector<size_t> equiv_classes(unrefined_left_edges.size(),
+                                 numeric_limits<size_t>::max());
+    size_t next_equiv_class = 0;
+    super.for_each_adjacent_side(center, [&](handle_t right) {
+        // refine the classes using the edges of this node
+        // TODO: this coud be done without an unordered_map by reseting
+        // a vector after every iterations
+        unordered_map<size_t, size_t> equiv_mapping;
+        super.for_each_adjacent_side(right, [&](handle_t left) {
+            size_t eq_class = equiv_classes[left_idx[left]];
+            auto it = equiv_mapping.find(eq_class);
+            if (it != equiv_mapping.end()) {
+                // we've already refined this class, just look it up
+                equiv_classes[left_idx[left]] = it->second;
+            }
+            else {
+                // refine with a new class
+                equiv_mapping[eq_class] = next_equiv_class;
+                equiv_classes[left_idx[left]] = next_equiv_class;
+                ++next_equiv_class;
+            }
+            return true;
+        });
+        return true;
+    });
+    
+    // quotient the nodes by the equivalence classes
+    left_edges.reserve(next_equiv_class - right_nodes.size());
+    vector<bool> quotiented(next_equiv_class, false);
+    for (size_t i = 0; i < unrefined_left_nodes.size(); ++i) {
+        size_t equiv_class = equiv_classes[i];
+        if (!quotiented[equiv_class]) {
+            // we're coming across this class for the first time, bogart
+            // its edges to be the edges of the quotient node
+            left_edges.emplace_back(move(unrefined_left_edges[i]));
+            quotiented[equiv_class] = true;;
+        }
+    }
+    
+    // add the leftward edges to the now refined left side
+    for (size_t i = 0; i < left_edges.size(); ++i) {
+        for (auto j : left_edges[i]) {
+            right_edges[j].push_back(i);
+        }
+    }
+}
+
+bool QuotientBall::check_neighbor_ordering_property() {
+    
+    // partition left nodes by their degree (T_x(k) in Amilhastre)
+    vector<vector<size_t>> degree_groups(right_edges.size());
+    for (size_t i = 0; i < left_edges.size(); ++i) {
+        degree_groups[left_edges[i].size()].push_back(i);
+    }
+    
+    // organize the neighborhoods of the right nodes in degree ordering
+    // (V(y) in Amilhastre)
+    vector<vector<size_t>> degree_ordered_nbds(right_edges.size());
+    for (const auto& degree_group : degree_groups) {
+        for (auto left : degree_group) {
+            for (auto right : left_edges[left]) {
+                degree_ordered_nbds[right].push_back(left);
+            }
+        }
+    }
+    
+    // immediate successor in the equiv class ordering (Succ in Amilhastre)
+    vector<size_t> successor(left_edges.size(), numeric_limits<size_t>::max());
+    // all immediate predecessors in the equiv class ordering (Gamma_x^-
+    // in Amilhastre)
+    vector<vector<size_t>> predecessors;
+    
+    // construct the predecessor lists and check for tree structure
+    for (size_t i = 0; i < right_edges.size(); ++i) {
+        auto& degree_ordered_nbd = degree_ordered_nbds[i];
+        size_t pred = degree_ordered_nbd.front();
+        for (size_t j = 1; j < degree_ordered_nbd.size(); ++j) {
+            size_t succ = degree_ordered_nbds[j];
+            if (successor[pred] != numeric_limits<size_t>::max()) {
+                successor[pred] = succ;
+                predecessors[succ].push_back(pred);
+            }
+            else if (successors[pred] != succ) {
+                // the successors don't form a tree
+                return false;
+            }
+            pred = succ;
+        }
+    }
+    
+    // check for the proper containent relationships between the neighborhoods
+    // of the nodes on the left
+    for (size_t i = 0; i < left_edges.size(); ++i) {
+        auto& succ_nbd = left_edges[i];
+        for (auto j : predecessors[i]) {
+            auto& pred_nbd = left_edges[j];
+            
+            // note: all of the left edge lists are constructed in sorted order
+            size_t p = 0;
+            for (size_t s = 0; s < succ_nbd.size() && p < pred_nbd.size(); ++s) {
+                if (succ_nbd[s] == pred_nbd[p]) {
+                    ++p;
+                }
+            }
+            if (p < pred_nbd.size()) {
+                // the neighborhoods weren't contained
+                return false;
+            }
+        }
+    }
+    
+    // we've passed all the checks
+    return true;
+}
 
 bool BicliqueCover::is_domino_free() const {
-    
+    bool domino_free = true;
+    // TODO: embarassingly parallel
+    for (size_t i = 0; i < left_partition.size() && domino_free; ++i) {
+        QuotientBall ball(*this, left_partition[i]);
+        domino_free = ball.check_neighbor_ordering_property();
+    }
+    return domino_free;
 }
 
 SubtractiveHandleGraph BicliqueCover::simplify() const {
@@ -68,9 +212,19 @@ SubtractiveHandleGraph BicliqueCover::simplify() const {
     return simplified;
 }
 
-vector<bipartition> BicliqueCover::domino_free_cover() const {
+GaloisLattice BicliqueCover::get_galois_lattice(const HandleGraph& simple_graph) const {
     
 }
+
+vector<bipartition> BicliqueCover::domino_free_cover() const {
+    // simplify the graph without affecting the biclique cover (Amilhastre, et al.
+    // 1998 algorithm 2).
+    SubtractiveHandleGraph simplified = simplify();
+    GaloisLattice galois_lattice = get_galois_lattice(simplified);
+    // TODO: get separator of galois lattice
+    // TODO: convert separator to biclique cover
+}
+
 vector<bipartition> BicliqueCover::heuristic_cover() const {
     
 }
@@ -88,7 +242,7 @@ void BicliqueCover::simplify_side(const vector<handle_t>& simplifying_partition,
     // keeps track of which nodes have successors (LI in Amilhastre, et al. 1998)
     vector<bool> nonmaximal(simplifying_partition.size(), false);
     
-    // matrix of successors (succ(u) in Amilhastre, et al. 1998)
+    // matrix of successors (succ(u) in Amilhastre)
     vector<vector<bool>> successor;
     successor.reserve(simplifying_partition.size());
     for (size_t i = 0; i < simplifying_partition.size(); ++i) {
@@ -198,12 +352,15 @@ void BicliqueCover::simplify_side(const vector<handle_t>& simplifying_partition,
                         if (neighbor_delta[j][k] == 0 && degree[j] > 0) {
                             // j's neighbors are now a subset of k's neighbors
                             nonmaximal[j] = true;
-                            successors[k][j] = true;
-                            ++num_successors[k];
+                            if (!successors[k][j]) {
+                                successors[k][j] = true;
+                                ++num_successors[k];
+                            }
                         }
                     }
                 });
             }
+            nonmaximal[i] = false;
         }
     }
 }
