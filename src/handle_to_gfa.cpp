d8678dd12245fedc3ab976b4cb1419ebb639a033
ryan <rlorigro@ucsc.edu>
2020-09-14 14:30:10 -0700

    Added minimal GFA writer

---
+++ src/handle_to_gfa.cpp
@@ -0,0 +1,65 @@
+#include "handle_to_gfa.hpp"
+
+
+namespace bluntifier {
+
+
+char get_reversal_character(const HandleGraph& graph, const handle_t& node){
+    bool reversed = graph.get_is_reverse(node);
+
+    if (reversed){
+        return '-';
+    }
+    else{
+        return '+';
+    }
+}
+
+
+void write_node_to_gfa(const HandleGraph& graph, const handle_t& node, ofstream& output_file){
+    output_file << "S\t" << graph.get_id(node) << '\t' << graph.get_sequence(node) << '\n';
+}
+
+
+void write_edge_to_gfa(const HandleGraph& graph, const edge_t& edge, ofstream& output_file){
+    output_file << "L\t" << graph.get_id(edge.first) << '\t' << get_reversal_character(graph, edge.first) << '\t'
+                << graph.get_id(edge.second) << '\t' << get_reversal_character(graph, edge.second) << '\t'
+                << "0M" << '\n';
+}
+
+
+/// With no consideration for directionality, just dump all the edges/nodes into GFA format
+void handle_graph_to_gfa(const HandleGraph& graph, const string& output_path){
+    ofstream output_gfa(output_path);
+
+    if (not output_gfa.good()){
+        throw runtime_error("ERROR: output file could not be written: " + output_path);
+    }
+
+    graph.for_each_handle([&](const handle_t& node){
+        write_node_to_gfa(graph, node, output_gfa);
+    });
+
+    graph.for_each_edge([&](const edge_t& edge){
+        write_edge_to_gfa(graph, edge, output_gfa);
+    });
+}
+
+
+// TODO write this method to use the overlaps and id map to write the linkages/sequences in the canonical direction
+// using the canonical names as well, wherever possible
+void handle_graph_to_canonical_gfa(const HandleGraph& graph, const string& output_path){
+    ofstream output_gfa(output_path);
+
+//    graph.for_each_handle([&](handle_t& node){
+//
+//    });
+//
+//    graph.for_each_edge([&](edge_t& edge){
+//
+//    });
+}
+
+
+
+}
---
+++ src/test/test_handle_to_gfa.cpp
@@ -0,0 +1,45 @@
+#include "bdsg/packed_graph.hpp"
+#include "gfa_to_handle.hpp"
+#include "handle_to_gfa.hpp"
+#include "utility.hpp"
+#include "IncrementalIdMap.hpp"
+#include "OverlapMap.hpp"
+
+using handlegraph::handle_t;
+using bdsg::PackedGraph;
+using std::ifstream;
+
+using bluntifier::gfa_to_path_handle_graph_in_memory;
+using bluntifier::gfa_to_path_handle_graph;
+using bluntifier::gfa_to_handle_graph;
+using bluntifier::handle_graph_to_gfa;
+using bluntifier::parent_path;
+using bluntifier::join_paths;
+using bluntifier::IncrementalIdMap;
+using bluntifier::OverlapMap;
+using bluntifier::Alignment;
+
+
+int main(){
+    string script_path = __FILE__;
+    string project_directory = parent_path(script_path, 3);
+
+    // Get test GFA path
+    string relative_gfa_path = "/data/test_gfa1.gfa";
+    string absolute_gfa_path = join_paths(project_directory, relative_gfa_path);
+
+    ifstream file(absolute_gfa_path);
+
+    PackedGraph g;
+    IncrementalIdMap<string> id_map;
+    OverlapMap overlaps;
+
+    gfa_to_handle_graph(absolute_gfa_path, g, id_map, overlaps);
+
+    string output_dir = join_paths(project_directory, "data/");
+    string output_path = join_paths(output_dir, "handle_to_gfa_test_output.gfa");
+    handle_graph_to_gfa(g, output_path);
+
+    return 0;
+}
+
