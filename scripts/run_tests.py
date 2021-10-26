2b578e15767fb52bbf149d7b70cab0510d325238
Ryan <rlorigro@ucsc.edu>
2021-10-26 13:06:24 -0700

    Fix bulk testing script

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -13,10 +13,12 @@ import subprocess
 import hashlib
 import time
 
+from collections import defaultdict
+
 if __name__ == "__main__":
     
     if len(sys.argv) != 3:
-        print("usage: ./run_tests.py bluntify_executable data_dir", file = sys.stderr)
+        print("usage: ./run_tests.py bluntify_executable data_dir", file=sys.stderr)
         sys.exit()
     
     here = os.path.dirname(os.path.realpath(__file__))
@@ -26,37 +28,66 @@ if __name__ == "__main__":
     datadir = sys.argv[2]
     
     tdir = tempfile.mktemp()
-    
+    os.makedirs(tdir)
+
+    all_pass = True
+    failing_tests = defaultdict(list)
+
     for filename in os.listdir(datadir):
         if not filename.endswith(".gfa") and not filename.endswith(".GFA"):
             continue
         
-        in_gfa = os.path.join(datadir, filename)
-        print("evaluating GFA {}".format(in_gfa), file = sys.stderr)
+        in_gfa_path = os.path.join(datadir, filename)
+        print("evaluating GFA {}".format(in_gfa_path), file=sys.stderr)
         
         prefix = os.path.join(tdir, os.path.basename(filename))
-        out_gfa = prefix + ".blunt.gfa"
-        out_table = prefix + ".table.txt"
-        bluntify_cmd = " ".join([bluntify, in_gfa, out_gfa, out_table])
-        print(bluntify_cmd)
-        validate_cmd = " ".join([validate, in_gfa, out_gfa, out_table])
-        print(validate_cmd)
-        #subprocess.check_call(cmd)
-    
-    num_random_gfas = 50
-    for i in range(num_random_gfas):
-        gfa_name = os.path.join(tdir, hashlib.sha256(str.encode(str(time.time()))).hexdigest()[:10] + ".gfa")
-        generate_cmd = generate + " > " + gfa_name
-        print("evaluating GFA {}".format(gfa_name), file = sys.stderr)
-        
-        prefix = os.path.join(tdir, os.path.basename(gfa_name))
-        out_gfa = prefix + ".blunt.gfa"
-        out_table = prefix + ".table.txt"
-        bluntify_cmd = " ".join([bluntify, in_gfa, out_gfa, out_table])
-        print(bluntify_cmd)
-        validate_cmd = " ".join([validate, in_gfa, out_gfa, out_table])
-        print(validate_cmd)
-        
-        
-        
-    
+        out_gfa_path = prefix + ".blunt.gfa"
+        out_table_path = prefix + ".table.txt"
+
+        with open(out_gfa_path, 'w') as out_gfa:
+            bluntify_cmd = [bluntify, in_gfa_path, "-p", out_table_path]
+            print(" ".join(bluntify_cmd))
+
+            try:
+                subprocess.run(bluntify_cmd, stdout=out_gfa, check=True)
+            except Exception as e:
+                print("FAIL")
+                print(e)
+
+                failing_tests[" ".join(bluntify_cmd)].append(e)
+                all_pass = False
+
+
+            validate_cmd = [validate, in_gfa_path, out_gfa_path, out_table_path]
+            print(" ".join(validate_cmd))
+
+            try:
+                subprocess.run(validate_cmd, stdout=out_gfa, check=True)
+            except Exception as e:
+                print(e)
+                failing_tests[" ".join(validate_cmd)].append(e)
+
+                all_pass = False
+
+    if not all_pass:
+        for item in failing_tests.items():
+            print("FAIL: ", item[0])
+            for subitem in item[1]:
+                print(subitem)
+            print()
+
+        exit("FAIL")
+
+    # num_random_gfas = 50
+    # for i in range(num_random_gfas):
+    #     gfa_name = os.path.join(tdir, hashlib.sha256(str.encode(str(time.time()))).hexdigest()[:10] + ".gfa")
+    #     generate_cmd = generate + " > " + gfa_name
+    #     print("evaluating GFA {}".format(gfa_name), file = sys.stderr)
+    #
+    #     prefix = os.path.join(tdir, os.path.basename(gfa_name))
+    #     out_gfa = prefix + ".blunt.gfa"
+    #     out_table = prefix + ".table.txt"
+    #     bluntify_cmd = " ".join([bluntify, in_gfa, out_gfa, out_table])
+    #     print(bluntify_cmd)
+    #     validate_cmd = " ".join([validate, in_gfa, out_gfa, out_table])
+    #     print(validate_cmd)
